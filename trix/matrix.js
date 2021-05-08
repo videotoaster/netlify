c14382109d62a9f83ac16c275b78d8056fce5296
videotoaster <52890399+videotoaster@users.noreply.github.com>
2021-05-08 11:36:06 -0500

    improve readability

--- trix/matrix.js
+++ trix/matrix.js
@@ -1,13 +1,15 @@
 function login(homeserv, username, password) {
-  // AJAX
+  // we need this thing because it does POST stuff
   var xhttp = new XMLHttpRequest();
-  // JSON to POST to API
+  
+  // we need this JSON to log in, matrix api will get very angry if we don't have it
   var data = {
      "type": "m.login.password",
      "user": username,
      "password": password
   }
-  // on return value, set token
+  
+  // on return value, set token to return (or alternatively an error)
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var json = JSON.parse(this.responseText);
@@ -18,16 +20,20 @@ function login(homeserv, username, password) {
       token = false;
     }
   };
+  
   // send HTTP request
   xhttp.open("POST", document.getElementById("matrix_homeserv").value+"/_matrix/client/r0/login", true);
   xhttp.send(JSON.stringify(data));
+  
   // return false/access token
   return token;
 }
 function main() {
-  // Get form data
+  // get form data to log in
   var hs = document.getElementById("matrix_homeserv").value;
   var un = document.getElementById("matrix_username").value;
   var pw = document.getElementById("matrix_password").value;
+  
+  // you know what this does
   var token = login(hs,un,pw);
 }
