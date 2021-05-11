973970019f3dc815362303ad4478f71b655016d2
videotoaster <52890399+videotoaster@users.noreply.github.com>
2021-05-11 09:36:35 -0500

    add more documentation, return true, and resize func

--- mediacanvas.js
+++ mediacanvas.js
@@ -1,3 +1,4 @@
+// Fullscreen canvas. Useful for games.
 // USAGE: mediacanvas_fullscreen(document.getElementById("myCanvas"));
 function mediacanvas_fullscreen(c) {
   // Resize canvas to window size
@@ -7,8 +8,21 @@ function mediacanvas_fullscreen(c) {
   // Remove HTML/BODY margin
   document.body.style.margin = 0;
   document.body.parentElement.style.margin = 0;
+  
+  return true;
 }
+
+// Set canvas cursor. Useful for UI elements.
 // USAGE: mediacanvas_setcursor(document.getElementById("myCanvas"),"pointer");
 function mediacanvas_setcursor(c,cursor) {
   c.style.cursor = cursor;
+  return true;
+}
+
+// Resize. Useful for resizing windows, perhaps.
+// USAGE: mediacanvas_resize(document.getElementById("myCanvas"),640,480);
+function mediacanvas_setcursor(c,w,h) {
+  c.width = w;
+  c.height = h;
+  return true;
 }
